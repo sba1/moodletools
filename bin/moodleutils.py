1b632718cc1267f56d5098fe993159187ba10dce
Sebastian Bauer <mail@sebastianbauer.info>
2020-08-25 01:10:17 +0200

    Add support for remote add and remote remove.

--- bin/moodleutils.py
+++ bin/moodleutils.py
@@ -1,10 +1,12 @@
 import configparser
+import getpass
 import os
+import sys
 import requests
 
 from xdg import BaseDirectory # type: ignore
 
-from typing import Dict
+from typing import Dict, Optional
 
 def config() -> Dict[str, Dict[str, str]]:
     """Returns the moodlecli configuration.
@@ -22,6 +24,55 @@ def config() -> Dict[str, Dict[str, str]]:
             dict[section][key] = config[section][key]
 
     return dict
+
+def store_config(config : Dict[str, Dict[str, str]]) -> None:
+    cfg = configparser.ConfigParser()
+    for section in config.keys():
+        cfg.add_section(section)
+        for key in config[section].keys():
+            cfg.set(section, key, config[section][key])
+    save_path = BaseDirectory.save_config_path('moodlecli')
+    os.makedirs(save_path, exist_ok=True)
+    with open(os.path.join(save_path, 'moodlecli.ini'), 'w') as cfgfile:
+        cfg.write(cfgfile)
+
+def config_resolve_key(config : Dict[str, Dict[str, str]], remote : str) -> Optional[str]:
+    """Tries to resolve the given remote name to the primary URL"""
+    if remote in config:
+        return remote
+
+    for r in config:
+        if config[r]['short'] == remote:
+            return r
+
+    return None
+
+def rem_remote(name:str) -> None:
+    cfg = config()
+    key = config_resolve_key(cfg, name)
+    if key is not None:
+        del cfg[key]
+        store_config(cfg)
+
+def get_token(url : str, service : str) -> Optional[str]:
+    """Query the given URL for the token for the given service."""
+    if not url.startswith('https://'):
+        print("An unsecured connection is about to be established, do you want to continue (y/N)? ", end='', flush=True)
+        answer = sys.stdin.readline().strip()
+        if answer != 'y':
+            return None
+
+    print("User: ", end='', flush=True)
+    username = sys.stdin.readline()
+    password = getpass.getpass()
+
+    page = requests.post(url + '/login/token.php',
+        data = {
+            'username': username,
+            'password': password,
+            'service': service,
+            })
+    return page.json()['token']
 
 def callws(config : Dict[str,str], remote : str, wsfunction : str, data : Dict[str,str] = {}):
     page = requests.post(remote + '/webservice/rest/server.php',
